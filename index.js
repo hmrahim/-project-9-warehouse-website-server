b743f9d1cecd1d1eb1cf866037d89aaffc450308
hmrahim <h.m.rahimnet@gmail.com>
2022-04-29 20:56:49 +0600

    some code modified

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT ||5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
+
+//app.use(cors)
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trq2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run =async ()=> {
+    await client.connect()
     try {
-        await client.connect()
     console.log("database connected");
         
     } finally{
-        
+
     }
 
 }
@@ -24,14 +27,11 @@ const run =async ()=> {
 run().catch(console.dir)
 
 
-app.use(cors)
-app.use(express.json())
-
 
 
 
 app.get("/",(req,res)=> {
-    res.send("hello home")
+    res.send("hello from home page")
 })
 
 
